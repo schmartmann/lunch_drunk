0649261ce65aeecb7120a848b32c556ed4231ca1
schmartmann <stefanhartmann@gmail.com>
2018-12-04 16:19:11 -0500

    add seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,34 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+breakfast = TimePeriod.create(
+  name: 'breakfast'
+)
+
+cereal_with_milk = Meal.create(
+  name: 'cereal with milk',
+  time_period: breakfast
+)
+
+milk = Ingredient.create(
+  name: 'milk',
+  unit: 'fl_oz',
+  quantity: 4
+)
+
+cereal = Ingredient.create(
+  name: 'Kashi Go Lean Crunch',
+  unit: 'g',
+  quantity: 156
+)
+
+MealIngredient.create(
+  meal: cereal_with_milk,
+  ingredient: milk
+)
+
+MealIngredient.create(
+  meal: cereal_with_milk,
+  ingredient: cereal
+)
