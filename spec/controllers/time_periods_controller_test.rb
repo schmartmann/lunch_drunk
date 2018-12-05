c70f5a4d3d9740d3b5639341aaaf73fbd50f687d
schmartmann <stefanhartmann@gmail.com>
2018-12-05 11:40:11 -0500

    add time periods controller test

--- spec/controllers/time_periods_controller_test.rb
+++ spec/controllers/time_periods_controller_test.rb
@@ -1,14 +1,9 @@
 require 'rails_helper'
 
-RSpec.describe TimePeriodsController do
-  let( :time_period ) { FactoryBot.create( :time_period ) }
+RSpec.describe "time_period #index", type: :request do
+  it 'valid request returns 200' do
+    time_period = FactoryBot.create( :time_period )
 
-  describe 'GET #index' do
-    context 'with valid attributes' do
-      it 'renders 200' do
-        get :index
-        expect( response.response_code ).to eq 200
-      end
-    end
+    get '/time_periods'
   end
 end
