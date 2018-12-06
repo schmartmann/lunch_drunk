a77a87451f4fb888bae013b7e1dae1c6ea7843d0
schmartmann <stefanhartmann@gmail.com>
2018-12-06 17:45:38 -0500

    reformat read helper

--- app/helpers/read_helper.rb
+++ app/helpers/read_helper.rb
@@ -1,10 +1,5 @@
 module ReadHelper
-
   def read_helper
-    existing_record =
-      constant
-        .where(
-          uuid: params[ :uuid ]
-        ).first
+    constant.where( uuid: params[ :uuid ] ).first
   end
 end
