61f1802c9a553ee419ed680e3cf646e954494ec8
schmartmann <stefanhartmann@gmail.com>
2018-12-07 12:57:58 -0500

    add working meal ingredients controller + tests

---
+++ app/controllers/meal_ingredients_controller.rb
@@ -0,0 +1,70 @@
+class MealIngredientsController < ApplicationController
+
+    PERMITTED_ATTRIBUTES = [
+      :uuid,
+      :meal_id,
+      :ingredient_id
+    ].freeze
+
+    def query
+      
+    end
+
+    def write
+      unless existing_meal_ingredient
+        meal_ingredient = MealIngredient.new(
+          meal_ingredient_params
+        )
+
+        if meal_ingredient.save
+          render json: {
+            meal_ingredient: existing_meal_ingredient
+          }
+        elsif meal_ingredient.errors.any?
+          render json: {
+            error: meal_ingredient.errors.full_messages
+          },
+          status: :unprocessable_entity
+        end
+      else
+        render json: {
+          meal_ingredient: existing_meal_ingredient
+        }
+      end
+    end
+
+    def destroy
+      meal_ingredient =
+        MealIngredient
+          .find_by( uuid: meal_ingredient_params[ :uuid ] )
+
+      if meal_ingredient && meal_ingredient.destroy
+        render json: {
+          message: "meal_ingredient #{ meal_ingredient.uuid } successfully destroyed"
+        }
+      else
+        not_found_error( params )
+      end
+    end
+
+    def existing_meal
+      @meal ||= Meal.find( meal_ingredient_params[ :meal_id ] ) rescue nil
+    end
+
+    def existing_ingredient
+      @ingredient ||= Ingredient.find( meal_ingredient_params[ :ingredient_id ] ) rescue nil
+    end
+
+    def existing_meal_ingredient
+      @meal_ingredient ||=
+        MealIngredient
+          .where(
+            meal: existing_meal,
+            ingredient: existing_ingredient
+          ).first
+    end
+
+    private; def meal_ingredient_params
+      params.require( :meal_ingredient ).permit( PERMITTED_ATTRIBUTES )
+    end
+end
---
+++ spec/requests/meal_ingredients_controller_spec.rb
@@ -0,0 +1,177 @@
+require 'rails_helper'
+
+RSpec.describe 'meal_ingredient controller', type: :request do
+  before( :all ) do
+    @time_period = FactoryBot.create( :time_period )
+    @meal        = FactoryBot.create( :meal, time_period: @time_period )
+    @ingredient  = FactoryBot.create( :ingredient )
+  end
+
+  describe '#write' do
+    context 'with valid attributes' do
+      it 'returns resource' do
+        params = {
+          meal_ingredient: {
+            meal_id: @meal.id,
+            ingredient_id: @ingredient.id
+          }
+        }
+
+        post '/meal_ingredients', params: params
+
+        meal_ingredient = JSON.parse( response.body )[ 'meal_ingredient' ]
+
+        expect( response.status ).to eq( 200 )
+        expect( meal_ingredient[ 'meal_id' ] ).to eq( @meal.id )
+        expect( meal_ingredient[ 'ingredient_id' ] ).to eq( @ingredient.id )
+      end
+    end
+
+    context 'with existing records' do
+      it 'returns record' do
+        @meal_ingredient = FactoryBot.create(
+          :meal_ingredient,
+          meal: @meal,
+          ingredient: @ingredient
+        )
+
+        uuid = @meal_ingredient.uuid
+
+        params = {
+          meal_ingredient: {
+            meal_id: @meal.id,
+            ingredient_id: @ingredient.id
+          }
+        }
+
+        post '/meal_ingredients', params: params
+
+        meal_ingredient = JSON.parse( response.body )[ 'meal_ingredient' ]
+
+        expect( response.status ).to eq( 200 )
+        expect( meal_ingredient[ 'uuid' ] ).to eq( uuid )
+      end
+
+      it 'associates meal + ingredient' do
+        @meal_ingredient = FactoryBot.create(
+          :meal_ingredient,
+          meal: @meal,
+          ingredient: @ingredient
+        )
+
+        meals_ingredients = @meal.ingredients.select { | i | i.uuid == @ingredient.uuid }
+        ingredients_meals = @ingredient.meals.select { | m | m.uuid == @meal.uuid }
+
+        expect( @meal.ingredients.any? ).to eq( true )
+        expect( @ingredient.meals.any? ).to eq( true )
+        expect( meals_ingredients.include?( @ingredient ) ).to eq( true )
+        expect( ingredients_meals.include?( @meal ) ).to eq( true )
+      end
+    end
+
+    context 'with invalid attributes' do
+      it 'returns nil' do
+        params = {
+          meal_ingredient: {
+            meal_id: nil,
+            ingredient_id: @ingredient.id
+          }
+        }
+
+        post '/meal_ingredients', params: params
+
+        error = JSON.parse( response.body )[ 'error' ]
+        expect( error.present? ).to eq( true )
+        expect( error.first ).to eq( 'Meal can\'t be blank' )
+        expect( error.last ).to eq( 'Meal must exist' )
+        expect( response.status ).to eq( 422 )
+      end
+
+      it 'returns nil' do
+        params = {
+          meal_ingredient: {
+            meal_id: @meal.id,
+            ingredient_id: nil
+          }
+        }
+
+        post '/meal_ingredients', params: params
+
+        error = JSON.parse( response.body )[ 'error' ]
+        expect( error.present? ).to eq( true )
+        expect( error.first ).to eq( 'Ingredient can\'t be blank' )
+        expect( error.last ).to eq( 'Ingredient must exist' )
+        expect( response.status ).to eq( 422 )
+      end
+
+      it 'returns nil' do
+        params = {
+          meal_ingredient: {
+            meal_uuid: nil,
+            ingredient_uuid: nil
+          }
+        }
+
+        post '/meal_ingredients', params: params
+
+        error = JSON.parse( response.body )[ 'error' ]
+        expected_errors = 'Meal can\'t be blank, Ingredient can\'t be blank,' +
+          ' Meal must exist, Ingredient must exist'
+
+        expect( error.present? ).to eq( true )
+        expect( error.join( ', ' ) ).to eq( expected_errors )
+        expect( response.status ).to eq( 422 )
+      end
+    end
+  end
+
+  describe '#destroy' do
+    before( :all ) do
+      @time_period      = FactoryBot.create( :time_period )
+      @meal             = FactoryBot.create( :meal, time_period: @time_period )
+      @ingredient       = FactoryBot.create( :ingredient )
+      @meal_ingredient  = FactoryBot.create( :meal_ingredient,
+                                             meal: @meal,
+                                             ingredient: @ingredient
+                                           )
+    end
+
+    context 'with valid attributes' do
+      it 'returns nil' do
+        uuid = @meal_ingredient.uuid
+
+        params = {
+          meal_ingredient: {
+            uuid: uuid
+          }
+        }
+
+        delete "/meal_ingredients/#{ uuid }", params: params
+
+        meal_ingredient =
+          MealIngredient.where(
+            uuid: uuid
+          )
+
+        expect( response.status ).to eq( 200 )
+        expect( meal_ingredient.blank? ).to eq( true )
+      end
+    end
+
+    context 'with invalid attributes' do
+      it 'returns 404' do
+        bad_uuid = SecureRandom.hex
+
+        params = {
+          meal_ingredient: {
+            uuid: bad_uuid
+          }
+        }
+
+        delete "/meal_ingredients/#{ bad_uuid }", params: params
+
+        expect( response.status ).to eq( 404 )
+      end
+    end
+  end
+end
