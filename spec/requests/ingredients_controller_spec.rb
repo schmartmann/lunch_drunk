11334a2fc15495b0d98ff59d26a512094d508d6a
schmartmann <stefanhartmann@gmail.com>
2018-12-17 18:39:54 -0500

    remove redundant test

--- spec/requests/ingredients_controller_spec.rb
+++ spec/requests/ingredients_controller_spec.rb
@@ -335,20 +335,4 @@ RSpec.describe 'ingredients controller', type: :request do
       end
     end
   end
-
-  context '#filter' do
-    context 'when uuids are valid' do
-      it 'returns list of associated meals' do
-        ingredient_ids = Ingredient.all.sample( 2 ).pluck( :id )
-
-        params = {
-          
-        }
-
-        get '/meal_ingredients', params: params
-
-        expect( response.status ).to eq( 200 )
-      end
-    end
-  end
 end
