6ca3c239fea2cbcff44a3effba2d8bf55049022c
schmartmann <stefanhartmann@gmail.com>
2019-01-14 16:48:08 -0500

    CORS issues

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   get    'time_periods',        to: 'time_periods#query',    as: 'time_periods'
-  get    'time_periods/:time_period_uuid',  to: 'time_periods#read',     as: 'time_period'
+  get    'time_periods/:uuid',  to: 'time_periods#read',     as: 'time_period'
   post   'time_periods',        to: 'time_periods#write',    as: 'write_time_period'
   delete 'time_periods/:uuid',  to: 'time_periods#destroy',  as: 'destroy_time_period'
 
